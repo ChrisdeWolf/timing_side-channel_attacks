6a4bdae5699a69ccaf19ba0766e23006371ddc86
Chris deWolf <cdewolf2015@ChristohersMBP3.fios-router.home>
2021-12-03 00:08:28 -0500

    stuff

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # timing_side-channel_attacks
 Implementations and examples of various timing-based side channel attacks.
+Run the python flask server and use the attack.py to launch a timing attack.
--- utilityScript.py
+++ utilityScript.py
@@ -1,14 +1,10 @@
 import requests
 import statistics
 
-
 inputPass = input("Password Guess: ")
-
 respTimes = []
 for i in range(10):
     response = requests.post('http://127.0.0.1:5000/login', data={'username':'admin','password':str(inputPass)})
     respTime = response.elapsed.total_seconds()
     respTimes.append(respTime)
-
-print('avg: ')
-print(statistics.mean(respTimes))
+print('avg: '+statistics.mean(respTimes))
